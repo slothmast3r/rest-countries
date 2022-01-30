4b9e69d528d802964ddafcff73941f6acaca507e
slothmast3r <oskar.straszynski@gmail.com>
2022-01-30 06:17:52 +0100

    gh pages

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const execa = require("execa");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require("fs");
 (async () => {
   try {
